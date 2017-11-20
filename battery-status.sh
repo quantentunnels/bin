a08c81a532e2fec4f00ee3777827d991c6c47277
Marius Hintsche <marius.hintsche@uni-potsdam.de>
2017-11-20 16:22:44 +0100

    fix battery status script

--- battery-status.sh
+++ battery-status.sh
@@ -2,6 +2,6 @@
 # parse acpi battery status message and shorten w/ unicode chars
 
 #tsv string look-up table
-lookup='battery-status.sed'
+lookup='/home/marius/bin/battery-status.sed'
 
-acpi | sed -f battery-status.sed
+acpi | sed -f $lookup
